b5b4c032466c19962036fa4a05172a2b8b8206e5
Aleale81 <69593297+Aleale81@users.noreply.github.com>
2021-09-10 14:52:26 +0200

    created a new weather react app, added  a new component Weather, axios and loader

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+
+    function handleResponce(responce) {
+        alert(`The temperature in ${props.city} is ${Math.round(responce.data.main.temp)}°C`)
+    }
+
+let apiKey = `a1bed3af7d71b323ab4af9036fcd943d`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponce);
+
+    return (
+        <div className="Weather">
+            <h2>Hello from {props.city}</h2>
+             <Loader
+        type="BallTriangle"
+        color= "gold"
+        height={100}
+        width={100}
+        timeout={3000} 
+      />
+        </div>);
+}
